3663fd23fc7ad8929db54bab0fa04b6a2dcfc078
neopage <neopage@users.noreply.github.com>
2021-07-22 23:20:51 +0900

    add vaccination card

---
+++ components/cards/VaccinationTotalCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <agency-bar-chart
+      :title="$t('ワクチン接種人数(合計)')"
+      :title-id="'agency'"
+      :chart-id="'agency'"
+      :chart-data="agencyData"
+      :date="agencyData.date"
+      :unit="$t('人')"
+    />
+  </v-col>
+</template>
+
+<script>
+import agencyData from '@/data/vaccination-total.json'
+import AgencyBarChart from '@/components/VaccinationTotal.vue'
+
+export default {
+  components: {
+    AgencyBarChart
+  },
+  data() {
+    return {
+      agencyData
+    }
+  }
+}
+</script>
---
+++ utils/colorsVaccinationTotal.ts
@@ -0,0 +1,117 @@
+export type SurfaceStyle = {
+  strokeColor: string
+  fillColor: string
+}
+
+const surfaceStyleA: SurfaceStyle = {
+  strokeColor: '#34a853',
+  fillColor: '#34a853'
+}
+
+const surfaceStyleB: SurfaceStyle = {
+  strokeColor: '#ff994d',
+  fillColor: '#ff994d'
+}
+
+const surfaceStyleC: SurfaceStyle = {
+  strokeColor: '#999999',
+  fillColor: '#999999'
+}
+
+const surfaceStyleD: SurfaceStyle = {
+  strokeColor: '#34a853',
+  fillColor: '#34a853'
+}
+const surfaceStyleE: SurfaceStyle = {
+  strokeColor: '#999999',
+  fillColor: '#999999'
+}
+const surfaceStyleF: SurfaceStyle = {
+  strokeColor: '#906ba3',
+  fillColor: '#906ba3'
+}
+const surfaceStyleG: SurfaceStyle = {
+  strokeColor: '#adb431',
+  fillColor: '#adb431'
+}
+const surfaceStyleH: SurfaceStyle = {
+  strokeColor: '#46bdc6',
+  fillColor: '#46bdc6'
+}
+const surfaceStyleI: SurfaceStyle = {
+  strokeColor: '#ef7b72',
+  fillColor: '#ef7b72'
+}
+const surfaceStyleJ: SurfaceStyle = {
+  strokeColor: '#7caaf7',
+  fillColor: '#7caaf7'
+}
+const surfaceStyleK: SurfaceStyle = {
+  strokeColor: '#f8d776',
+  fillColor: '#f8d776'
+}
+const surfaceStyleL: SurfaceStyle = {
+  strokeColor: '#fc4fd0',
+  fillColor: '#fc4fd0'
+}
+const surfaceStyleM: SurfaceStyle = {
+  strokeColor: '#71c387',
+  fillColor: '#71c387'
+}
+const surfaceStyleN: SurfaceStyle = {
+  strokeColor: '#ff994d',
+  fillColor: '#ff994d'
+}
+const surfaceStyleO: SurfaceStyle = {
+  strokeColor: '#555555',
+  fillColor: '#555555'
+}
+const surfaceStyleP: SurfaceStyle = {
+  strokeColor: '#bbbbbb',
+  fillColor: '#bbbbbb'
+}
+const surfaceStyleQ: SurfaceStyle = {
+  strokeColor: '#0f2f4a',
+  fillColor: '#0f2f4a'
+}
+const surfaceStyleR: SurfaceStyle = {
+  strokeColor: '#8e2f7a',
+  fillColor: '#8e2f7a'
+}
+const surfaceStyleS: SurfaceStyle = {
+  strokeColor: '#222222',
+  fillColor: '#222222'
+}
+
+export function getGraphSeriesStyle(seriesLength: number) {
+  switch (seriesLength) {
+    case 1:
+      return [surfaceStyleB]
+    case 2:
+      return [surfaceStyleA, surfaceStyleC]
+    case 3:
+      return [surfaceStyleA, surfaceStyleB, surfaceStyleC]
+    default:
+      return [
+        surfaceStyleA,
+        surfaceStyleB,
+        surfaceStyleC,
+        surfaceStyleD,
+        surfaceStyleE,
+        surfaceStyleF,
+        surfaceStyleG,
+        surfaceStyleH,
+        surfaceStyleI,
+        surfaceStyleJ,
+        surfaceStyleK,
+        surfaceStyleL,
+        surfaceStyleM,
+        surfaceStyleN,
+        surfaceStyleO,
+        surfaceStyleP,
+        surfaceStyleQ,
+        surfaceStyleR,
+        surfaceStyleS
+      ]
+  }
+}
